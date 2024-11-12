5ddcf94e7b34de7912e5e393ef33172c331ffd70
oskari6 <oskarisulkakoski@gmail.com>
2024-11-12 14:07:32 +0200

    map

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -1,71 +1,37 @@
 "use client";
 
-import Container from "@mui/material/Container";
-import * as React from "react";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Chart from "./components/Chart";
-import Deposits from "./components/Deposits";
+import React, { useEffect } from "react";
 import Orders from "./components/Orders";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
+const mapContainerStyle = {
+  width: "100%",
+  height: "400px",
+};
+
+const center = {
+  lat: 40.7128,
+  lng: -74.006,
+};
+
+const MapComponent = () => {
+  return (
+    <LoadScript
+      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}
+    >
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        center={center}
+        zoom={10}
+      ></GoogleMap>
+    </LoadScript>
+  );
+};
 export default function Home() {
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-      <Grid container spacing={3}>
-        {/* Chart */}
-        <Grid item xs={12} md={8} lg={9}>
-          <Paper
-            sx={{
-              p: 2,
-              display: "flex",
-              flexDirection: "column",
-              height: 240,
-            }}
-          >
-            <Chart />
-          </Paper>
-        </Grid>
-        {/* Recent Deposits */}
-        <Grid item xs={12} md={4} lg={3}>
-          <Paper
-            sx={{
-              p: 2,
-              display: "flex",
-              flexDirection: "column",
-              height: 240,
-            }}
-          >
-            <Deposits />
-          </Paper>
-        </Grid>
-        {/* Recent Orders */}
-        <Grid item xs={12}>
-          <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-            <Orders />
-          </Paper>
-        </Grid>
-      </Grid>
-      <Copyright sx={{ pt: 4 }} />
-    </Container>
-  );
-}
-
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
+    <div>
+      <h1>map</h1>
+      <MapComponent />
+    </div>
   );
 }
