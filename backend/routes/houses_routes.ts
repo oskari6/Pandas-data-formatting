1e17eab196ef7170750d359b8082acb0c53cd480
Oskari <148843965+oskari6@users.noreply.github.com>
2025-06-28 13:51:06 +0300

    Delete backend directory

--- backend/routes/houses_routes.ts
+++
@@ -1,8 +0,0 @@
-import { getAllHouses } from "../controllers/houses_controllers";
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/houses", getAllHouses);
-
-export default router;
